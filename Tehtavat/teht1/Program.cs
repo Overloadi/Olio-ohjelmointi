e533862da478bdded7fc860ffbef88f0e84322fd
Joni <Joni@DESKTOP-M2EUSGA>
2017-01-24 20:58:40 +0200

    initial commit

---
+++ Tehtavat/teht1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku > ");
+            string input;
+            input = Console.ReadLine();
+            switch (input)
+            {
+                case "1": Console.WriteLine("Annoit luvun yksi"); break;
+                case "2": Console.WriteLine("Annoit luvun kaksi"); break;
+                case "3": Console.WriteLine("Annoit luvun kolme"); break;
+                default: Console.WriteLine("Annoit jonkun muun luvun"); break;
+            }
+        }
+    }
+}
